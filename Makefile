8585ddc9463781e96f11093153f2e3ebb6a254a8
Daniel Nyström <sp2danny@gmail.com>
2024-11-04 01:58:05 +0100

    update

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ fd - Debug ]----------"
+	@echo "----------Building project:[ fd - Release ]----------"
 	@"$(MAKE)" -f  "fd.mk"
 clean:
-	@echo "----------Cleaning project:[ fd - Debug ]----------"
+	@echo "----------Cleaning project:[ fd - Release ]----------"
 	@"$(MAKE)" -f  "fd.mk" clean
--- fd.mk
+++ fd.mk
@@ -2,21 +2,20 @@
 ## Auto Generated makefile by CodeLite IDE
 ## any manual changes will be erased      
 ##
-## Debug
+## Release
 ProjectName            :=fd
-ConfigurationName      :=Debug
-WorkspaceConfiguration :=Debug
+ConfigurationName      :=Release
+WorkspaceConfiguration :=Release
 WorkspacePath          :=/home/daniel/project/ddec2
 ProjectPath            :=/home/daniel/project/ddec2
-IntermediateDirectory  :=./bin/Debug
-OutDir                 :=$(IntermediateDirectory)
+IntermediateDirectory  :=./bin/Release
+OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=daniel
 Date                   :=04/11/24
 CodeLitePath           :=/home/daniel/.codelite
-MakeDirCommand         :=mkdir -p
 LinkerName             :=/usr/bin/clang++-15
 SharedObjectLinkerName :=/usr/bin/clang++-15 -shared -fPIC
 ObjectSuffix           :=.o
@@ -29,14 +28,15 @@ OutputSwitch           :=-o
 LibraryPathSwitch      :=-L
 PreprocessorSwitch     :=-D
 SourceSwitch           :=-c 
-OutputDirectory        :=.
-OutputFile             :=./$(ProjectName)_dbg.out
-Preprocessors          :=
+OutputDirectory        :=
+OutputFile             :=$(ProjectName).out
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
 PreprocessOnlySwitch   :=-E
-ObjectsFileList        :=$(IntermediateDirectory)/ObjectsList.txt
+ObjectsFileList        :="fd.txt"
 PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
 LinkOptions            := -std=c++2b 
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -52,8 +52,8 @@ LibPath                := $(LibraryPathSwitch).
 AR       := /usr/bin/ar rcu
 CXX      := /usr/bin/clang++-15
 CC       := /usr/bin/clang-15
-CXXFLAGS := -std=c++2b -fmodules -Wall -Wextra -Werror -pedantic -Wno-multichar -Wno-pre-c++2b-compat -g -O0 $(Preprocessors)
-CFLAGS   :=  -g -O0 $(Preprocessors)
+CXXFLAGS := -std=c++2b -fmodules -Wall -Wextra -Werror -pedantic -Wno-multichar -Wno-pre-c++2b-compat -O3 $(Preprocessors)
+CFLAGS   :=  -O3 $(Preprocessors)
 ASFLAGS  := 
 AS       := /usr/bin/as
 
@@ -72,20 +72,20 @@ Objects=$(Objects0)
 ## Main Build Targets 
 ##
 .PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
-all: MakeIntermediateDirs $(OutputFile)
+all: $(OutputFile)
 
 $(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
-	@$(MakeDirCommand) "$(IntermediateDirectory)"
+	@$(MakeDirCommand) $(@D)
 	@echo "" > $(IntermediateDirectory)/.d
 	@echo $(Objects0)  > $(ObjectsFileList)
 	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
 
 MakeIntermediateDirs:
-	@$(MakeDirCommand) "$(IntermediateDirectory)"
-	@$(MakeDirCommand) "$(OutputDirectory)"
+	@test -d ./bin/Release || $(MakeDirCommand) ./bin/Release
+
 
 $(IntermediateDirectory)/.d:
-	@$(MakeDirCommand) "$(IntermediateDirectory)"
+	@test -d ./bin/Release || $(MakeDirCommand) ./bin/Release
 
 PreBuild:
 
@@ -93,43 +93,33 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/src_bitstream.cpp$(ObjectSuffix): src/bitstream.cpp $(IntermediateDirectory)/src_bitstream.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/bitstream.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitstream.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/src_bitstream.cpp$(DependSuffix): src/bitstream.cpp
+$(IntermediateDirectory)/src_bitstream.cpp$(ObjectSuffix): src/bitstream.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_bitstream.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_bitstream.cpp$(DependSuffix) -MM src/bitstream.cpp
-
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/bitstream.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitstream.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_bitstream.cpp$(PreprocessSuffix): src/bitstream.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_bitstream.cpp$(PreprocessSuffix) src/bitstream.cpp
 
-$(IntermediateDirectory)/src_fod.cpp$(ObjectSuffix): src/fod.cpp $(IntermediateDirectory)/src_fod.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/fod.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_fod.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/src_fod.cpp$(DependSuffix): src/fod.cpp
+$(IntermediateDirectory)/src_fod.cpp$(ObjectSuffix): src/fod.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_fod.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_fod.cpp$(DependSuffix) -MM src/fod.cpp
-
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/fod.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_fod.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_fod.cpp$(PreprocessSuffix): src/fod.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_fod.cpp$(PreprocessSuffix) src/fod.cpp
 
-$(IntermediateDirectory)/src_mersenne-twister.cpp$(ObjectSuffix): src/mersenne-twister.cpp $(IntermediateDirectory)/src_mersenne-twister.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/mersenne-twister.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_mersenne-twister.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/src_mersenne-twister.cpp$(DependSuffix): src/mersenne-twister.cpp
+$(IntermediateDirectory)/src_mersenne-twister.cpp$(ObjectSuffix): src/mersenne-twister.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_mersenne-twister.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_mersenne-twister.cpp$(DependSuffix) -MM src/mersenne-twister.cpp
-
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/mersenne-twister.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_mersenne-twister.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_mersenne-twister.cpp$(PreprocessSuffix): src/mersenne-twister.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_mersenne-twister.cpp$(PreprocessSuffix) src/mersenne-twister.cpp
 
-$(IntermediateDirectory)/src_util.cpp$(ObjectSuffix): src/util.cpp $(IntermediateDirectory)/src_util.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/util.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_util.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/src_util.cpp$(DependSuffix): src/util.cpp
+$(IntermediateDirectory)/src_util.cpp$(ObjectSuffix): src/util.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_util.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_util.cpp$(DependSuffix) -MM src/util.cpp
-
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/util.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_util.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_util.cpp$(PreprocessSuffix): src/util.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_util.cpp$(PreprocessSuffix) src/util.cpp
 
-$(IntermediateDirectory)/src_fd.cpp$(ObjectSuffix): src/fd.cpp $(IntermediateDirectory)/src_fd.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/fd.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_fd.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/src_fd.cpp$(DependSuffix): src/fd.cpp
+$(IntermediateDirectory)/src_fd.cpp$(ObjectSuffix): src/fd.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_fd.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_fd.cpp$(DependSuffix) -MM src/fd.cpp
-
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/daniel/project/ddec2/src/fd.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_fd.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_fd.cpp$(PreprocessSuffix): src/fd.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_fd.cpp$(PreprocessSuffix) src/fd.cpp
 
@@ -139,6 +129,6 @@ $(IntermediateDirectory)/src_fd.cpp$(PreprocessSuffix): src/fd.cpp
 ## Clean
 ##
 clean:
-	$(RM) -r $(IntermediateDirectory)
+	$(RM) -r ./bin/Release/
 
 
